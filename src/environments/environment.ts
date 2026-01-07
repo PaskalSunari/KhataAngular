de98bf701d4a2c8c3bc408e65cc55b2f0f81a4ce
Zarathushtra <paskalsunari@gmail.com>
2026-01-07 16:52:41 +0545

    local environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ declare const require: any;
 export const environment = {
   production: false,
 
-  //appURL: 'https://api.khatasystem.com/api/',
+  // appURL: 'https://api.khatasystem.com/api/',
   
 
   appURL:'https://localhost:7291/api/',
