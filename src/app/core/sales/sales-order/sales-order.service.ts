3679962edad46fca302e203c7bbd2c71761788d1
Zarathushtra <paskalsunari@gmail.com>
2026-01-19 16:38:49 +0545

    changes

--- src/app/core/sales/sales-order/sales-order.service.ts
+++ src/app/core/sales/sales-order/sales-order.service.ts
@@ -10,13 +10,18 @@ import { salesOrderUrl } from './sales-order-endpoint';
 export class salesOrderService {
   baseurl = environment.appURL;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private endpoint: salesOrderUrl) { }
 
   getSalesLedger(branchId: string){
      const base = this.baseurl.replace(/\/+$/, '');
     const params = new HttpParams()
     .set('flag', '1')
     .set('branchId', branchId);
-    return this.http.get(`${base}/SalesOrder/SalesLedger`,{params})
+    return this.http.get(`${base}${this.endpoint.salesLedgerDropdownList}`,{params})
+  }
+
+  getProductName(flag: any,code:any,branchId:any){
+    const base = this.baseurl.replace(/\/+$/, '');
+    return this.http.get(`${base}${this.endpoint.getProductByName}${flag}&code=${code}&branchId=${branchId}`);
   }
 }
