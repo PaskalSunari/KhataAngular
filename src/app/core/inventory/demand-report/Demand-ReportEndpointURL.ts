76f693995afc2c223213a27f97e6672006f9ce2b
Zarathushtra <paskalsunari@gmail.com>
2026-01-22 16:33:09 +0545

    chnage

---
+++ src/app/core/inventory/demand-report/Demand-ReportEndpointURL.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DemandReportEndpointURL {
+   locationList :string= `Inventory/DemandReport/LocationList?branchId=`
+ 
+}
--- src/app/core/inventory/demand-report/Service/demand-report.service.ts
+++ src/app/core/inventory/demand-report/Service/demand-report.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { DemandReportEndpointURL } from '../Demand-ReportEndpointURL';
+import { environment } from 'src/environments/environment';
+import { DemandReport } from '../demand-report.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DemandReportService {
 
-  constructor() { }
+branchID=localStorage.getItem('branch') || '1001';
+  baseurl = environment.appURL;
+  demandReportModel: DemandReport = new DemandReport();
+  constructor(private http: HttpClient, private endPoint: DemandReportEndpointURL) {}
+
+
+  getLocationDropdownList() {
+    return this.http.get(`${this.baseurl}${this.endPoint.locationList}${this.branchID}`);
+  }
 }
